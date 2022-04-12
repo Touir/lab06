36af66e94609d162831b42236b6cdfc660a3ec28
Touir <dima.minchenkov.88@mail.ru>
2022-04-12 11:20:15 +0300

    added CMakes

---
+++ solver_application/_build/CMakeFiles/Solver.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Solver.dir/equation.cpp.o"
+  "CMakeFiles/Solver.dir/equation.cpp.o.d"
+  "Solver"
+  "Solver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Solver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
